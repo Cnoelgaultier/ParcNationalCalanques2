d1fe2eba5639f6fd186a7e7462d1b1c6b74c7636
Camille Noel Gaultier <noelgaultiercamille@gmail.Com>
2026-03-18 09:05:31 +0100

    Merge pull request #4 from Cnoelgaultier/reservation
    Reservation

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,16 @@
 module.exports = function (api) {
     api.cache(true);
     return {
+<<<<<<< HEAD
         presets: [
             ['babel-preset-expo', { jsxImportSource: 'nativewind' }]
         ],
+=======
+reservation
+        presets: [
+            ['babel-preset-expo', { jsxImportSource: 'nativewind' }]
+        ],
+
+>>>>>>> 8c0d88f (Merge pull request #4 from Cnoelgaultier/reservation)
     };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< HEAD
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+=======
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+>>>>>>> 8c0d88f (Merge pull request #4 from Cnoelgaultier/reservation)
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
+<<<<<<< HEAD
 }
+=======
+};
+>>>>>>> 8c0d88f (Merge pull request #4 from Cnoelgaultier/reservation)
