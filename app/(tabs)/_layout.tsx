9df352ed961316ea269e17183b1a113e4188ee42
Maxence Etienne <maxence.etienne.pro@gmail.com>
2026-04-01 10:36:50 +0200

    Revert "Authentification"

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,19 +27,9 @@ export default function TabLayout() {
                 name="createReservation"
                 options={{
                     title: 'Réserver',
-                    //tabBarIcon: ({ color }) => <IconSymbol size={28} name="calendar.fill" color={color} />,
+                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="calendar.fill" color={color} />,
                 }}
             />
-            <Tabs.Screen 
-                name="signup" 
-                options={{ 
-                    title: "S'inscrire" 
-                }} />
-            <Tabs.Screen 
-                name="login" 
-                options={{ 
-                    title: "Se connecter" 
-                }} />
         </Tabs>
     );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,15 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import "../global.css";
-import { AuthProvider } from './context/AuthContext';
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  return (
-    <AuthProvider>
-      <QueryClientProvider client={queryClient}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-      </QueryClientProvider>
-    </AuthProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
+        </QueryClientProvider>
+    );
 }
